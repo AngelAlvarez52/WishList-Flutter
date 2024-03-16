bf5aec72dbf19f6d713bcab5e0afcddc271ec050
AngelAlvarez52 <a22311093@uthermosillo.edu.mx>
2024-03-16 16:17:19 -0700

    El usuario puede editar y eliminar el regalo

---
+++ proyecto/lib/GiftEditPage.dart
@@ -0,0 +1,245 @@
+// ignore_for_file: use_build_context_synchronously
+import 'dart:convert';
+import 'dart:typed_data';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:image_picker/image_picker.dart';
+import 'package:logger/logger.dart';
+
+final Logger _logger = Logger();
+
+class GiftEditPage extends StatefulWidget {
+  final String authToken;
+  final int giftId;
+
+  const GiftEditPage({Key? key, required this.authToken, required this.giftId})
+      : super(key: key);
+
+  @override
+  GiftEditPageState createState() => GiftEditPageState();
+}
+
+class GiftEditPageState extends State<GiftEditPage> {
+  late Future<Map<String, dynamic>> _giftDataFuture;
+  TextEditingController nameController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+  TextEditingController urlController = TextEditingController();
+  TextEditingController priceController = TextEditingController();
+  int? _selectedCategoryId;
+  int? _selectedShopId;
+  List<Map<String, dynamic>> _categories = [];
+  List<Map<String, dynamic>> _shops = [];
+  String? _imageUrl;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchCategories();
+    _fetchShops();
+    _giftDataFuture = _fetchGiftData();
+  }
+
+  Future<Map<String, dynamic>> _fetchGiftData() async {
+    final response = await http
+        .get(Uri.parse('http://127.0.0.1:8000/api/Gifts/${widget.giftId}'));
+
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception('Failed to load gift data');
+    }
+  }
+
+  Future<void> _fetchCategories() async {
+    final response =
+        await http.get(Uri.parse('http://127.0.0.1:8000/api/categories'));
+
+    if (response.statusCode == 200) {
+      final List<dynamic> data = jsonDecode(response.body);
+      setState(() {
+        _categories =
+            data.map((category) => category as Map<String, dynamic>).toList();
+      });
+    } else {
+      throw Exception('Failed to load categories');
+    }
+  }
+
+  Future<void> _fetchShops() async {
+    final response =
+        await http.get(Uri.parse('http://127.0.0.1:8000/api/Shops'));
+
+    if (response.statusCode == 200) {
+      final List<dynamic> data = jsonDecode(response.body);
+      setState(() {
+        _shops = data.map((shop) => shop as Map<String, dynamic>).toList();
+      });
+    } else {
+      throw Exception('Failed to load shops');
+    }
+  }
+
+  Future<void> _updateGift() async {
+    final url =
+        Uri.parse('http://127.0.0.1:8000/api/Gifts/${widget.giftId}/update');
+
+    final request = http.MultipartRequest('POST', url);
+    request.headers.addAll({
+      'Accept': 'application/json',
+      'Authorization': 'Bearer ${widget.authToken}',
+    });
+
+    request.fields['id'] = widget.giftId.toString();
+    request.fields['name'] = nameController.text;
+    request.fields['description'] = descriptionController.text;
+    request.fields['url'] = urlController.text;
+    request.fields['price'] = priceController.text;
+    request.fields['category_id'] = _selectedCategoryId.toString();
+    request.fields['shop_id'] = _selectedShopId.toString();
+
+    // Adjuntar la imagen al formulario
+    if (_imageUrl != null) {
+      request.fields['image'] = _imageUrl!;
+    }
+
+    final response = await request.send();
+
+    if (response.statusCode == 200) {
+      _logger.i('Gift added successfully');
+      _logger.i(await response.stream.bytesToString());
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Se actualizo el regalo'),
+        ),
+      );
+      Navigator.pop(context,
+          true); // Regresar a la página anterior y notificar que se agregó un regalo
+    } else {
+      _logger.e('No se pudo actualizar el reglo');
+      _logger.e(await response.stream.bytesToString());
+    }
+  }
+
+  Future<void> _pickImage() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      setState(() {
+        _imageUrl = pickedFile.path;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Editar Regalo'),
+      ),
+      body: FutureBuilder<Map<String, dynamic>>(
+        future: _giftDataFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else {
+            final giftData = snapshot.data!;
+            nameController.text = giftData['name'];
+            descriptionController.text = giftData['description'];
+            urlController.text = giftData['url'];
+            priceController.text = giftData['price'].toString();
+            _selectedCategoryId = giftData['category_id'];
+            _selectedShopId = giftData['shop_id'];
+
+            return SingleChildScrollView(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  TextField(
+                    controller: nameController,
+                    decoration: const InputDecoration(labelText: 'Nombre'),
+                  ),
+                  const SizedBox(height: 16),
+                  TextField(
+                    controller: descriptionController,
+                    decoration: const InputDecoration(labelText: 'Descripción'),
+                  ),
+                  const SizedBox(height: 16),
+                  TextField(
+                    controller: urlController,
+                    decoration: const InputDecoration(labelText: 'URL'),
+                  ),
+                  const SizedBox(height: 16),
+                  TextField(
+                    controller: priceController,
+                    decoration: const InputDecoration(labelText: 'Precio'),
+                  ),
+                  const SizedBox(height: 16),
+                  ElevatedButton(
+                    onPressed: _pickImage,
+                    child: const Text('Subir Imagen'),
+                  ),
+                  const SizedBox(height: 16),
+                  if (_imageUrl != null)
+                    Image.network(
+                      _imageUrl!,
+                      height: 200,
+                    ),
+                  const SizedBox(height: 16),
+                  DropdownButtonFormField<int>(
+                    value: _selectedCategoryId,
+                    onChanged: (newValue) {
+                      setState(() {
+                        _selectedCategoryId = newValue;
+                      });
+                    },
+                    items: [
+                      const DropdownMenuItem<int>(
+                        value: null,
+                        child: Text('Escoge una categoría'),
+                      ),
+                      for (final category in _categories)
+                        DropdownMenuItem<int>(
+                          value: category['id'],
+                          child: Text(category['name']),
+                        )
+                    ],
+                  ),
+                  const SizedBox(height: 16),
+                  DropdownButtonFormField<int>(
+                    value: _selectedShopId,
+                    onChanged: (newValue) {
+                      setState(() {
+                        _selectedShopId = newValue;
+                      });
+                    },
+                    items: [
+                      const DropdownMenuItem<int>(
+                        value: null,
+                        child: Text('Escoge una tienda'),
+                      ),
+                      for (final shop in _shops)
+                        DropdownMenuItem<int>(
+                          value: shop['id'],
+                          child: Text(shop['name']),
+                        )
+                    ],
+                  ),
+                  const SizedBox(height: 16),
+                  ElevatedButton(
+                    onPressed: _updateGift,
+                    child: const Text('Actualizar Regalo'),
+                  ),
+                ],
+              ),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
--- proyecto/lib/UserGiftPage.dart
+++ proyecto/lib/UserGiftPage.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:logger/logger.dart';
 import 'GiftAddPage.dart';
+import 'GiftEditPage.dart';
 
 class UserGiftPage extends StatefulWidget {
   final String authToken;
@@ -16,6 +18,7 @@ class UserGiftPage extends StatefulWidget {
 class UserGiftPageState extends State<UserGiftPage> {
   late Future<List<Gift>> _giftsFuture;
   Gift? _selectedGift;
+  final Logger _logger = Logger();
 
   @override
   void initState() {
@@ -55,11 +58,66 @@ class UserGiftPageState extends State<UserGiftPage> {
     }
   }
 
+  Future<List<Comment>> _fetchComments(int giftId) async {
+    final response = await http.get(
+      Uri.parse('http://127.0.0.1:8000/api/comments_gift?gift_id=$giftId'),
+      headers: {
+        'Authorization': 'Bearer ${widget.authToken}',
+      },
+    );
+    if (response.statusCode == 200) {
+      final List<dynamic> jsonData = jsonDecode(response.body);
+      return jsonData.map((json) => Comment.fromJson(json)).toList();
+    } else {
+      throw Exception(
+          'Failed to load comments. Status code: ${response.statusCode}');
+    }
+  }
+
+  Future<List<User>> _fetchUsers(List<Comment> comments) async {
+    final List<int> userIds =
+        comments.map((comment) => comment.userId).toList();
+    final List<Future<User>> userFutures =
+        userIds.map((userId) => _fetchUser(userId)).toList();
+    return await Future.wait(userFutures);
+  }
+
+  Future<User> _fetchUser(int userId) async {
+    final response = await http.get(
+      Uri.parse('http://127.0.0.1:8000/api/Users/$userId'),
+      headers: {
+        'Authorization': 'Bearer ${widget.authToken}',
+      },
+    );
+    if (response.statusCode == 200) {
+      final jsonData = jsonDecode(response.body);
+      return User.fromJson(jsonData);
+    } else {
+      throw Exception(
+          'Failed to load user. Status code: ${response.statusCode}');
+    }
+  }
+
+  Future<void> eliminarRegalo(int giftId) async {
+    final response = await http.delete(
+      Uri.parse('http://127.0.0.1:8000/api/Gifts/delete/$giftId'),
+      headers: {
+        'Authorization': 'Bearer ${widget.authToken}',
+      },
+    );
+    if (response.statusCode == 200) {
+      setState(() {
+        _selectedGift = null;
+        _giftsFuture = _fetchGifts();
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(_selectedGift != null ? _selectedGift!.name : 'User Gifts'),
+        title: Text(_selectedGift != null ? _selectedGift!.name : 'Mis deseos'),
         actions: <Widget>[
           IconButton(
             icon: const Icon(Icons.add),
@@ -71,7 +129,6 @@ class UserGiftPageState extends State<UserGiftPage> {
                       GiftAddPage(authToken: widget.authToken),
                 ),
               );
-              // If a gift was added, refresh the list
               if (result == true) {
                 setState(() {
                   _giftsFuture = _fetchGifts();
@@ -81,7 +138,26 @@ class UserGiftPageState extends State<UserGiftPage> {
           ),
         ],
       ),
-      body: _selectedGift != null ? _buildGiftDetails() : _buildGiftsList(),
+      body: SingleChildScrollView(
+        child: _selectedGift != null
+            ? Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  _buildGiftDetails(),
+                  const SizedBox(height: 20),
+                  const Text(
+                    'Comentarios',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  _buildCommentsSection(),
+                ],
+              )
+            : _buildGiftsList(),
+      ),
     );
   }
 
@@ -96,6 +172,7 @@ class UserGiftPageState extends State<UserGiftPage> {
         } else {
           final List<Gift> gifts = snapshot.data!;
           return ListView.builder(
+            shrinkWrap: true,
             padding: const EdgeInsets.all(5),
             itemCount: gifts.length,
             itemBuilder: (context, index) {
@@ -113,10 +190,11 @@ class UserGiftPageState extends State<UserGiftPage> {
                     fit: BoxFit.cover,
                   ),
                 ),
-                onTap: () {
+                onTap: () async {
                   setState(() {
                     _selectedGift = gift;
                   });
+                  _loadComments(gift.id);
                 },
               );
             },
@@ -127,61 +205,138 @@ class UserGiftPageState extends State<UserGiftPage> {
   }
 
   Widget _buildGiftDetails() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        Expanded(
-          flex: 2,
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Image.network(
-              _selectedGift!.image,
-              fit: BoxFit.cover,
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Image.network(
+            _selectedGift!.image,
+            fit: BoxFit.cover,
+          ),
+          const SizedBox(height: 20),
+          Text(
+            _selectedGift!.name,
+            style: const TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
             ),
           ),
-        ),
-        Expanded(
-          flex: 3,
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  _selectedGift!.name,
-                  style: const TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(height: 10),
-                Text(
-                  _selectedGift!.description,
-                  style: const TextStyle(
-                    fontSize: 16,
-                  ),
-                ),
-                const SizedBox(height: 10),
-                Text(
-                  '\$${_selectedGift!.price.toStringAsFixed(2)}',
-                  style: const TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.green,
-                  ),
-                ),
-                const SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () {
-                    _launchURL(_selectedGift!.url);
-                  },
-                  child: const Text('Ir al regalo'),
-                ),
-              ],
+          const SizedBox(height: 10),
+          Text(
+            _selectedGift!.description,
+            style: const TextStyle(
+              fontSize: 16,
             ),
           ),
-        ),
-      ],
+          const SizedBox(height: 10),
+          Text(
+            '\$${_selectedGift!.price.toStringAsFixed(2)}',
+            style: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.green,
+            ),
+          ),
+          const SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  _launchURL(_selectedGift!.url);
+                },
+                child: const Text('Ir al regalo'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => GiftEditPage(
+                        authToken: widget.authToken,
+                        giftId: _selectedGift!.id,
+                      ),
+                    ),
+                  );
+                },
+                child: const Text('Editar Regalo'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: const Text('Eliminar Regalo'),
+                        content: const Text(
+                            '¿Estás seguro de que deseas eliminar este regalo?'),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: const Text('Cancelar'),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              eliminarRegalo(_selectedGift!.id);
+                              Navigator.of(context).pop();
+                            },
+                            child: const Text('Eliminar'),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
+                child: const Text('Eliminar regalo'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildCommentsSection() {
+    return FutureBuilder<List<Comment>>(
+      future: _fetchComments(_selectedGift!.id),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return Center(child: Text('Error: ${snapshot.error}'));
+        } else {
+          final List<Comment> comments = snapshot.data!;
+          return FutureBuilder<List<User>>(
+            future: _fetchUsers(comments),
+            builder: (context, userSnapshot) {
+              if (userSnapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              } else if (userSnapshot.hasError) {
+                return Center(child: Text('Error: ${userSnapshot.error}'));
+              } else {
+                final List<User> users = userSnapshot.data!;
+                return Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: List.generate(comments.length, (index) {
+                      final comment = comments[index];
+                      final user = users[index];
+                      return ListTile(
+                        title: Text('${user.name} ${user.surname}'),
+                        subtitle: Text(comment.text),
+                      );
+                    }),
+                  ),
+                );
+              }
+            },
+          );
+        }
+      },
     );
   }
 
@@ -191,6 +346,14 @@ class UserGiftPageState extends State<UserGiftPage> {
       await launchUrl(uri);
     } else {
       throw 'Could not launch $url';
+    }
+  }
+
+  void _loadComments(int giftId) async {
+    try {
+      setState(() {});
+    } catch (e) {
+      _logger.e('Error loading comments: $e');
     }
   }
 }
@@ -220,6 +383,40 @@ class Gift {
       url: json['url'],
       price: double.parse(json['price'].toString()),
       image: json['image'],
+    );
+  }
+}
+
+class Comment {
+  final int userId;
+  final String text;
+
+  Comment({
+    required this.userId,
+    required this.text,
+  });
+
+  factory Comment.fromJson(Map<String, dynamic> json) {
+    return Comment(
+      userId: json['user_id'],
+      text: json['text'],
+    );
+  }
+}
+
+class User {
+  final String name;
+  final String surname;
+
+  User({
+    required this.name,
+    required this.surname,
+  });
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      name: json['name'],
+      surname: json['surname'],
     );
   }
 }
